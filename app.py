21091e774bca3954cac6344ae677a77ede4d1e76
weihanlairrr <141799681+weihanlairrr@users.noreply.github.com>
2024-07-25 22:59:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ def main():
     option = ui.tabs(options=["每頁商品數「固定」的情形", "每頁商品數「不固定」的情形"], default_value="每頁商品數「固定」的情形")
 
     with st.sidebar:
-        st.image("91APP_logo.png")
+        st.image("Image/91APP_logo.png")
         with st.expander("文件上傳"):
             pdf_file = st.file_uploader("上傳 PDF", type=["pdf"])
             data_file = st.file_uploader("上傳 CSV 或 XLSX", type=["csv", "xlsx"])
