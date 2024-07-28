2f431e0a009444d8ab103323537373c39ad66215
weihanlairrr <141799681+weihanlairrr@users.noreply.github.com>
2024-07-28 15:16:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ with st.sidebar:
         """,
         unsafe_allow_html=True
     )
-st.markdown('<div class="centered"><h3>PDF截圖與AI文案生成工具</h3></div>', unsafe_allow_html=True)
+st.markdown('<div class="centered"><h2>商運 PDF截圖與AI文案生成工具</h2></div>', unsafe_allow_html=True)
 st.write("\n")
 
 def create_directories():
